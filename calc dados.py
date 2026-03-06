e8247b62f17132eea5e15e49d57c36cc30db0cdd
yasminpossiano68 <yasminpossiano68@gmail.com>
2026-03-06 19:30:01 -0300

    Update calc dados.py

--- calc dados.py
+++ calc dados.py
@@ -14,7 +14,18 @@ def mae(v1, v2):
 def mse(v1, v2):
     validar_vetores(v1, v2)
     return sum((a - b) ** 2 for a, b in zip(v1, v2)) / len(v1)
+    
+# Distância Euclidiana
+def euclidiana(v1, v2):
+    validar_vetores(v1, v2)
+    soma_quadrados = sum((a - b) ** 2 for a, b in zip(v1, v2))
+    return soma_quadrados ** 0.5
 
+
+# Produto Escalar
+def produto_escalar(v1, v2):
+    validar_vetores(v1, v2)
+    return sum(a * b for a, b in zip(v1, v2))
 
 if _name_ == "_main_":
     obs = [1.2, 2.5, 3.8]
