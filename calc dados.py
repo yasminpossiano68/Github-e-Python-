dbaaad37dc4bf052b44243b17699a1cd0be6904c
YASMIN FERREIRA POSSIANO <47806528806@info.local>
2026-03-06 19:27:52 -0300

    segunda versao

--- calc dados.py
+++ calc dados.py
@@ -4,19 +4,20 @@ def validar_vetores(v1, v2):
     return True
 
 
-# MAE - Erro Médio Absoluto
-def mae(v1, v2):
+# Distância Euclidiana
+def euclidiana(v1, v2):
     validar_vetores(v1, v2)
-    return sum(abs(a - b) for a, b in zip(v1, v2)) / len(v1)
+    soma_quadrados = sum((a - b) ** 2 for a, b in zip(v1, v2))
+    return soma_quadrados ** 0.5
 
 
-# MSE - Erro Quadrático Médio
-def mse(v1, v2):
+# Produto Escalar
+def produto_escalar(v1, v2):
     validar_vetores(v1, v2)
-    return sum((a - b) ** 2 for a, b in zip(v1, v2)) / len(v1)
+    return sum(a * b for a, b in zip(v1, v2))
 
 
-if _name_ == "_main_":
+if __name__ == "_main_":
     obs = [1.2, 2.5, 3.8]
     pred = [1.0, 2.7, 4.0]
 
