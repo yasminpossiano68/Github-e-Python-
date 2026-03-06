0c54c3a898ca202896635f1786fa723c68ea5dfa
YASMIN FERREIRA POSSIANO <47806528806@info.local>
2026-03-06 19:25:18 -0300

    primeira versao

---
+++ calc dados.py
@@ -0,0 +1,24 @@
+def validar_vetores(v1, v2):
+    if len(v1) != len(v2):
+        raise ArithmeticError("Os vetores devem ter o mesmo tamanho!")
+    return True
+
+
+# Distância Euclidiana
+def euclidiana(v1, v2):
+    validar_vetores(v1, v2)
+    soma_quadrados = sum((a - b) ** 2 for a, b in zip(v1, v2))
+    return soma_quadrados ** 0.5
+
+
+# Produto Escalar
+def produto_escalar(v1, v2):
+    validar_vetores(v1, v2)
+    return sum(a * b for a, b in zip(v1, v2))
+
+
+if __name__== "_main_":
+    obs = [1.2, 2.5, 3.8]
+    pred = [1.0, 2.7, 4.0]
+
+    print("Sistema iniciado com sucesso.")
